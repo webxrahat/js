ed514b2020ab555805bdea0c441a35e9a56729a5
Md Rahat Islam <islammdrahat308@gmail.com>
2024-11-10 07:42:03 +0600

    rr

--- main.js
+++ main.js
@@ -1,5 +1,37 @@
-btn = document.getElementById("new-task");
-function get_vaue() {
-    console.log(btn.value);
-    console.log(btn.value);
-}
+let newTask = document.querySelector("#new-task");
+// console.log(newTask);
+let form = document.querySelector("form");
+// console.log(form);
+
+let toDoUl = document.querySelector("#items");
+// console.log(toDoUl);
+
+let compelteUl = document.querySelector(".complete-list ul");
+// console.log(compelteUl);
+
+let createTask = function (task) {
+    let listItem = document.createElement("li");
+    let checkBox = document.createElement("input");
+    let label = document.createElement("label");
+
+    label.innerHTML = task;
+    checkBox.type = "checkbox";
+
+    listItem.appendChild(checkBox);
+    listItem.appendChild(label);
+
+    return listItem;
+};
+
+let addTask = function (e) {
+    e.preventDefault();
+    let listItem = createTask(newTask.value);
+    toDoUl.appendChild(listItem);
+    newTask.value = "";
+};
+
+let addBtn = document.getElementById("btn");
+let todoContainer = document.getElementsByClassName("div-container");
+let inputField = document.getElementById("text-field");
+
+console.log(addBtn, todoContainer, inputField);
