a668c3813d569dab42b5ca3548eefc623ebaee10
Md Rahat Islam <islammdrahat308@gmail.com>
2024-11-11 07:03:24 +0600

    ree

---
+++ fatch.js
@@ -0,0 +1,8 @@
+fetch("https://jsonplaceholder.typicode.com/todos/1")
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+function loadData() {
+    fetch("https://jsonplaceholder.typicode.com/todos/1")
+        .then((response) => response.json())
+        .then((json) => console.log(json));
+}
