a28765354a3e15ed52980afe318155cf6d5ac798
Md Rahat Islam <islammdrahat308@gmail.com>
2024-11-13 20:06:24 +0600

    rr

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,14 +1,28 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
     const [count, setCount] = useState(0);
 
+    function getClick() {
+        const increase = count + 1;
+        return setCount(increase);
+    }
+
+    function minusValue() {
+        const dicrease = count - 1;
+
+        if (dicrease === -1) {
+            return 0;
+        } else return setCount(dicrease);
+    }
+
     return (
         <>
             <div>this is react app</div>
+            <p>Here is count: {count}</p>
+            <button onClick={getClick}>+</button>
+            <button onClick={minusValue}>-</button>
         </>
     );
 }
