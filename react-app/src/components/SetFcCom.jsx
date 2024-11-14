f1c2184726cc2837e5c12eeb588b84592d0ec9f3
Md Rahat Islam <islammdrahat308@gmail.com>
2024-11-14 07:51:18 +0600

    rr

--- react-app/src/components/SetFcCom.jsx
+++ react-app/src/components/SetFcCom.jsx
@@ -1,12 +1,5 @@
 import React from "react";
-import { SetCount } from "./SetCount";
 
 export const SetFcCom = () => {
-    const [count, setCount] = useState(0);
-
-    return (
-        <div>
-            <SetCount count={count} setCount={setCount} />
-        </div>
-    );
+    return <div></div>;
 };
