4c06989abc1ac8307c41740bca07bfec59c99785
Md Rahat Islam <islammdrahat308@gmail.com>
2024-11-15 22:45:29 +0600

    rr

--- react-app/src/components/header/Nav.jsx
+++ react-app/src/components/header/Nav.jsx
@@ -1,5 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
+// import logo from "../assets/logo.jpg";
 
 export const Nav = () => {
-    return <div>Nav</div>;
+    return (
+        <div>
+            <div>
+                <img src="" alt="img" />
+            </div>
+            <div>
+                <Link to="/">Home</Link>
+                <Link to="/about">About</Link>
+            </div>
+        </div>
+    );
 };
