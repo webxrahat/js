832dd19003e8e09566e9a119f3e1ef94cad6eb62
Md Rahat Islam <islammdrahat308@gmail.com>
2024-11-19 20:15:13 +0600

    tt

--- react-app/src/components/pages/About.jsx
+++ react-app/src/components/pages/About.jsx
@@ -1,5 +1,28 @@
 import React from "react";
 
 export const About = () => {
-    return <div>About</div>;
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const data = e.target;
+        let nameValue = data.name.value;
+        let emailValue = data.email.value;
+        console.log(nameValue, emailValue);
+    };
+
+    return (
+        <div>
+            <div>
+                <h2>Submit Form</h2>
+            </div>
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="name" />
+                <br />
+                <br />
+                <input type="email" name="email" />
+                <br />
+                <br />
+                <input type="submit" value="Submit" />
+            </form>
+        </div>
+    );
 };
