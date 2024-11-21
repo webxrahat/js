a918a6aa54fc28df225b4795dee01de941342687
Md Rahat Islam <islammdrahat308@gmail.com>
2024-11-21 21:54:16 +0600

    ht

--- server/index.js
+++ server/index.js
@@ -1,13 +1,28 @@
 const express = require("express");
+const phones = require("../server/phones.json");
 const app = express();
 const port = 5000;
 
+// console.log(phones);
+
 app.get("/", (req, res) => {
-    res.send("Hello world");
+    res.send("Hello world this is ture");
 });
 
 app.get("/new", (req, res) => {
-    res.send("New Line");
+    res.send("New Line new ");
+});
+
+app.get("/phone", (req, res) => {
+    res.send(phones);
+});
+
+app.get("/phone/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const signlePhone = phones.find((phone) => phone.id === id) || {};
+    res.send(signlePhone);
+
+    // console.log(id);
 });
 
 app.listen(port, () => {
