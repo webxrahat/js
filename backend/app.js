8fde66cd58e829ac75e6b3402692e51908a76ec3
Md Rahat Islam <islammdrahat308@gmail.com>
2024-11-27 06:34:10 +0600

    gd

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,38 @@ const app = express();
 const users = require("../backend/users.json");
 const port = process.env.port || 5000;
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 app.use(cors());
 app.use(express.json());
+
+const uri =
+    "mongodb+srv://roko:loQ5G6777kHMYW4C@cluster0.jhexa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log(
+            "Pinged your deployment. You successfully connected to MongoDB!"
+        );
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
     res.send("Database is runing on");
