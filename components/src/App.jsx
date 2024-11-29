52b7a15f8c41e9cc7c475b1c47b042cc67ff876e
Md Rahat Islam <islammdrahat308@gmail.com>
2024-11-29 08:41:21 +0600

    gt

---
+++ components/src/App.jsx
@@ -0,0 +1,10 @@
+import "./App.css";
+
+export default function App() {
+    return (
+        <div className="">
+            <h1 className="text-3xl font-bold underline">Hello world!</h1>
+            <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        </div>
+    );
+}
