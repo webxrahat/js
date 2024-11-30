f2c20597d9afeede17de43d2c92b9dd8dd80b65b
Md Rahat Islam <islammdrahat308@gmail.com>
2024-11-30 23:01:11 +0600

    ggf

--- components/src/App.jsx
+++ components/src/App.jsx
@@ -1,12 +1,12 @@
 import "./App.css";
+import Slider from "./components/Slider";
 
 export default function App() {
-    return (
-        <div className="flex justify-center align-middle items-center h-[70vh] w-full">
-            <div className="max-w-6xl bg-slate-200 mx-auto w-full h-[50vh]">
-                <h1 className="text-3xl font-bold underline">Hello world!</h1>
-                <h1 className="text-3xl font-bold underline">Hello world!</h1>
-            </div>
-        </div>
-    );
+ return (
+  <div className="flex justify-center align-middle items-center h-[70vh] w-full">
+   <div className="max-w-6xl bg-slate-200 mx-auto w-full h-[50vh]">
+    <Slider />
+   </div>
+  </div>
+ );
 }
