6c9c5d35d31eca6c7e9741fe2ab9f51838f9bc55
Md Rahat Islam <islammdrahat308@gmail.com>
2024-11-30 21:16:51 +0600

    ghy

--- components/src/App.jsx
+++ components/src/App.jsx
@@ -2,9 +2,11 @@ import "./App.css";
 
 export default function App() {
     return (
-        <div className="">
-            <h1 className="text-3xl font-bold underline">Hello world!</h1>
-            <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <div className="flex justify-center align-middle items-center h-[70vh] w-full">
+            <div className="max-w-6xl bg-slate-200 mx-auto w-full h-[50vh]">
+                <h1 className="text-3xl font-bold underline">Hello world!</h1>
+                <h1 className="text-3xl font-bold underline">Hello world!</h1>
+            </div>
         </div>
     );
 }
