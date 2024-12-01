c31b66a4b93f85feb72130dc5971504f48256996
Md Rahat Islam <islammdrahat308@gmail.com>
2024-12-01 07:47:35 +0600

    rr

--- components/src/components/Slider.jsx
+++ components/src/components/Slider.jsx
@@ -1,6 +1,6 @@
 import React, { useRef, useState } from "react";
 // Import Swiper React components
-import { Swiper, SwiperSlide } from "swiper/react";
+import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
 
 // Import Swiper styles
 import "swiper/css";
@@ -8,31 +8,60 @@ import "swiper/css/pagination";
 import "swiper/css/navigation";
 
 // import required modules
-import { Pagination, Navigation } from "swiper/modules";
+import { Pagination, Navigation, Autoplay } from "swiper/modules";
+import img1 from "../../photos/drone.webp";
+import img2 from "../../photos/earpod.jpeg";
+import img3 from "../../photos/bluepen.webp";
 
 export default function Slider() {
+ const SwiperButtonNext = ({ children }) => {
+  const swiper = useSwiper();
+  return <button onClick={() => swiper.slideNext()}>{children}</button>;
+ };
+ const SwiperButtonPre = ({ children }) => {
+  const swiper = useSwiper();
+  return <button onClick={() => swiper.slidePrev()}>{children}</button>;
+ };
+
+ const ref = useRef();
+ console.log(ref);
+
+ const photos = [
+  {
+   img: img1,
+  },
+  {
+   img: img2,
+  },
+  {
+   img: img3,
+  },
+ ];
+
  return (
   <>
+   <div></div>
    <Swiper
+    autoplay={{
+     delay: 2000,
+     disableOnInteraction: false,
+    }}
     slidesPerView={1}
-    spaceBetween={30}
     loop={true}
     pagination={{
      clickable: true,
     }}
-    navigation={true}
-    modules={[Pagination, Navigation]}
-    className="mySwiper"
+    // navigation={true}
+    modules={[Pagination, Autoplay]}
+    // className="mySwiper"
    >
-    <SwiperSlide className="bg-yellow-200 h-96">Slide 1</SwiperSlide>
-    <SwiperSlide>Slide 2</SwiperSlide>
-    <SwiperSlide>Slide 3</SwiperSlide>
-    <SwiperSlide>Slide 4</SwiperSlide>
-    <SwiperSlide>Slide 5</SwiperSlide>
-    <SwiperSlide>Slide 6</SwiperSlide>
-    <SwiperSlide>Slide 7</SwiperSlide>
-    <SwiperSlide>Slide 8</SwiperSlide>
-    <SwiperSlide>Slide 9</SwiperSlide>
+    <SwiperButtonNext>Next Slide</SwiperButtonNext>
+    <SwiperButtonPre>Pre Slide</SwiperButtonPre>
+    {photos.map((photo) => (
+     <SwiperSlide className="bg-yellow-200">
+      <img src={photo.img} alt="1" className="w-full h-[600px] object-cover" />
+     </SwiperSlide>
+    ))}
    </Swiper>
   </>
  );
